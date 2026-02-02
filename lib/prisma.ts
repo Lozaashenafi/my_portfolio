26ee695a6ec9ce3a946b7778d15834b2839a8bd3
Lozaashenafi <134071796+Lozaashenafi@users.noreply.github.com>
2026-02-02 17:05:49 +0300

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,17 @@
+// lib/prisma.ts
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-// This connection string comes from your .env file
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
 
+// Create the connection pool
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// This ensures we only have one instance of Prisma running in development
+// Initialize Prisma with the adapter
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
